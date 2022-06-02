1680f05c3b58f449cc4d493a8271e26cbd7e8cac
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2022-06-02 19:52:45 +0300

    first commit v2

--- yazLab/Form1.Designer.cs
+++ yazLab/Form1.Designer.cs
@@ -39,84 +39,96 @@ namespace yazLab
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // webBrowser1
             // 
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(15, 16);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1115, 1102);
+            this.webBrowser1.Size = new System.Drawing.Size(836, 895);
             this.webBrowser1.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1306, 31);
+            this.textBox1.Location = new System.Drawing.Point(980, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(257, 22);
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(1229, 31);
+            this.label1.Location = new System.Drawing.Point(922, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.Size = new System.Drawing.Size(51, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Enlem:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(1306, 71);
+            this.textBox2.Location = new System.Drawing.Point(980, 58);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(257, 22);
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
             this.textBox2.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(1229, 71);
+            this.label2.Location = new System.Drawing.Point(922, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Boylam:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(1306, 112);
+            this.textBox3.Location = new System.Drawing.Point(980, 91);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(257, 22);
+            this.textBox3.Size = new System.Drawing.Size(194, 20);
             this.textBox3.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(1229, 112);
+            this.label3.Location = new System.Drawing.Point(922, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.Size = new System.Drawing.Size(47, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Etiket:";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(1222, 164);
+            this.dataGridView1.Location = new System.Drawing.Point(916, 133);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(610, 258);
+            this.dataGridView1.Size = new System.Drawing.Size(458, 210);
             this.dataGridView1.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(1615, 67);
+            this.button1.Location = new System.Drawing.Point(1211, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 29);
+            this.button1.Size = new System.Drawing.Size(59, 24);
             this.button1.TabIndex = 4;
             this.button1.Text = "Bul";
             this.button1.UseVisualStyleBackColor = true;
@@ -125,19 +137,46 @@ namespace yazLab
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button2.Location = new System.Drawing.Point(1744, 67);
+            this.button2.Location = new System.Drawing.Point(1308, 54);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(79, 29);
+            this.button2.Size = new System.Drawing.Size(59, 24);
             this.button2.TabIndex = 4;
             this.button2.Text = "Sil";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(916, 515);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(458, 308);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(1081, 440);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 71);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "En kısa yol";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1902, 1055);
+            this.ClientSize = new System.Drawing.Size(1426, 857);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -148,12 +187,13 @@ namespace yazLab
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.webBrowser1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gezgin Satıcı";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +211,8 @@ namespace yazLab
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- yazLab/Form1.cs
+++ yazLab/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,11 +15,102 @@ namespace yazLab
     public partial class Form1 : Form
     {
         DataTable tablo = new DataTable();
+        DataTable tablo2 = new DataTable();
         int count = 0;
+        const double PI = Math.PI;
+
+        Dictionary<string, string> points = new Dictionary<string, string>();
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private double rad2Deg(double radians)
+        {
+            return radians * 180 / PI; 
+        }
+
+        private double deg2Rad(double degress)
+        {
+            return degress * PI / 180;
+        }
+        private double GetDistanceBetweenTwoPoint(double latitude1, double longitude1, double latitude2, double longitude2, string unit = "kilometers")
+        {
+            double theta = longitude1 - longitude2;
+            double distance = 60 * 1.1515 * rad2Deg(Math.Acos(((Math.Sin(deg2Rad(latitude1))) * Math.Sin(deg2Rad(latitude2))) + (Math.Cos(deg2Rad(latitude1)) * (Math.Cos(deg2Rad(latitude2)) * (Math.Cos(deg2Rad(theta)))))));
+
+            return Math.Round(distance * 1.609344, 2);
+        }
+
+        private void DrawGridViewHead(DataTable table)
+        {
+            table.Columns.Add("-");
+            foreach (var point in points)
+            {
+                table.Columns.Add(point.Key);
+            }
+        }
+
+        private void DrawDistancesGridview()
+        {
+
+            tablo2 = new DataTable();
+            DrawGridViewHead(tablo2);
+            
+            dataGridView2.DataSource = tablo2;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            CalcMeasurePoints();
+        }
+
+        private void CalcMeasurePoints()
+        {
+            double[,] distances = new double[points.Count(), points.Count()];
+            int row = 0, col = 0;
+
+            foreach (var firstPoint in points)
+            {
+                col = 0;
+
+                double firstPointLongLeft = Double.Parse(firstPoint.Value.Split('-')[0].Split('.')[0]);
+                double firstPointLongRight = Double.Parse(firstPoint.Value.Split('-')[0].Split('.')[1]) / Math.Pow(10, firstPoint.Value.Split('-')[0].Split('.')[1].Length);
+                double firstPointLong = firstPointLongLeft + firstPointLongRight;
+
+                double firstPointLatLeft = Double.Parse(firstPoint.Value.Split('-')[1].Split('.')[0]);
+                double firstPointLatRight = Double.Parse(firstPoint.Value.Split('-')[1].Split('.')[1]) / Math.Pow(10, firstPoint.Value.Split('-')[1].Split('.')[1].Length);
+                double firstPointLat = firstPointLatLeft + firstPointLatRight;
+
+                Object[] dgwRow = new object[points.Count() + 1];
+
+                dgwRow[0] = firstPoint.Key;
+
+
+                foreach (var secondPoint in points)
+                {
+                    
+
+                    if (firstPoint.Key != secondPoint.Key)
+                    {
+                        double secondPointLongLeft = Double.Parse(secondPoint.Value.Split('-')[0].Split('.')[0]);
+                        double secondPointLongRight = Double.Parse(secondPoint.Value.Split('-')[0].Split('.')[1]) / Math.Pow(10, secondPoint.Value.Split('-')[0].Split('.')[1].Length);
+                        double secondPointLong = secondPointLongLeft + secondPointLongRight;
+
+                        double secondPointLatLeft = Double.Parse(secondPoint.Value.Split('-')[1].Split('.')[0]);
+                        double secondPointLatRight = Double.Parse(secondPoint.Value.Split('-')[1].Split('.')[1]) / Math.Pow(10, secondPoint.Value.Split('-')[1].Split('.')[1].Length);
+                        double secondPointLat = secondPointLatLeft + secondPointLatRight;
+
+
+                        double distance = GetDistanceBetweenTwoPoint(firstPointLat, firstPointLong, secondPointLat, secondPointLong);
+                        distances[row, col] = distance;
+                        dgwRow[col + 1] = distance;
+                    }
+                    else dgwRow[col + 1] = '-';
+
+                    col++;
+                }
+                tablo2.Rows.Add(dgwRow);
+                row++;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -61,10 +154,15 @@ namespace yazLab
                 dataGridView1.DataSource = tablo;
                 //listBox1.Items.Add(txtLat.Text);
                 webBrowser1.Navigate(queryAddress.ToString());
+                
+                points.Add(textBox3.Text, textBox1.Text + "-" + textBox2.Text);
+
+                if (points.Count() > 1) DrawDistancesGridview();
 
                 textBox1.Text = "";
                 textBox2.Text = "";
                 textBox3.Text = "";
+
             }
             catch (Exception ex)
             {
@@ -82,6 +180,11 @@ namespace yazLab
             {
                 MessageBox.Show("Lüffen silinecek satırı seçin.","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
